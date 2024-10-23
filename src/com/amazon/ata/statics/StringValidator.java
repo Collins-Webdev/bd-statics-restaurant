5e335cb04c15bf607044aa5402fcfe9ecee3b0ac
Collins-Webdev <collinshaya@gmail.com>
2024-10-23 09:34:35 +0100

    success

--- src/com/amazon/ata/statics/StringValidator.java
+++ src/com/amazon/ata/statics/StringValidator.java
@@ -2,26 +2,36 @@ package com.amazon.ata.statics;
 
 public final class StringValidator {
 
-    // Create a private constructor so StringValidator objects cannot be created.
+    // Constant for the forbidden word
+    private static final String FORBIDDEN_WORD = "fish";
+
+    // Constant for the maximum allowed length
+    private static final int MAX_LENGTH = 250;
+
+    // Private constructor to prevent instantiation of the class
     private StringValidator() {}
 
     /**
      * Checks to see if the string entered contains a forbidden string.
      *
      * @param input string that's being checked
-     * @throws InvalidStringException if the input contains a forbidden string
+     * @throws InvalidStringException if the input contains the forbidden string
      */
     public static void containsForbiddenWord(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String contains a forbidden word.");
+        if (input.toLowerCase().contains(FORBIDDEN_WORD)) {
+            throw new InvalidStringException("The given String contains a forbidden word: '" + FORBIDDEN_WORD + "'.");
+        }
     }
 
     /**
      * Checks to see if the string entered has a length greater than the max allowed length.
      *
      * @param input string that's being checked
-     * @throws InvalidStringException checks to see if the input is a forbidden string
+     * @throws InvalidStringException if the input exceeds the maximum allowed length
      */
     public static void greaterThanMaxLength(String input) throws InvalidStringException {
-        throw new InvalidStringException("The given String has a length greater than the max length allowed.");
+        if (input.length() > MAX_LENGTH) {
+            throw new InvalidStringException("The given String exceeds the maximum allowed length of " + MAX_LENGTH + " characters.");
+        }
     }
 }
